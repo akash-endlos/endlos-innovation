e7ab0ab07ba15d1f640e7a5bc1fbe9e142a6be0f
akash-endlos <akash.endlos@gmail.com>
2023-05-10 18:19:35 +0530

    updated

--- src/app/all-content/rvmProducts.js
+++ src/app/all-content/rvmProducts.js
@@ -9,30 +9,30 @@ export const rvmProducts = [
         title:"RVM Pro",
         subtitle:"The fiber-based solid-state laser, TruFiber, is the precision laser for delicate work. It stands out with its single-mode beam quality and the variety of power classes available. While the lower power classes of the fiber laser (up to 1 kW) are optimally suited for laser precision cutting and welding, the higher power classes (over 1 kW) shine when it comes to wobble welding.",
         link:"/products/reverse-vending-machines/RVM-PRO",
-        src:"/images/RVM_pro_729x344_1.jpg"
+        src:"/images/RVM_pro_765x430_6.jpg"
      },
      {
         title:"RVM Premium",
         subtitle:"The fiber-based solid-state laser, TruFiber, is the precision laser for delicate work. It stands out with its single-mode beam quality and the variety of power classes available. While the lower power classes of the fiber laser (up to 1 kW) are optimally suited for laser precision cutting and welding, the higher power classes (over 1 kW) shine when it comes to wobble welding.",
         link:"/products/reverse-vending-machines/RVM-PREMIUM",
-        src:"/images/RVM_premium_729x344_1.jpg"
+        src:"/images/RVM_premium_765x430_2.jpg"
      },
      {
         title:"RVM DRS 11",
         subtitle:"The fiber-based solid-state laser, TruFiber, is the precision laser for delicate work. It stands out with its single-mode beam quality and the variety of power classes available. While the lower power classes of the fiber laser (up to 1 kW) are optimally suited for laser precision cutting and welding, the higher power classes (over 1 kW) shine when it comes to wobble welding.",
         link:"/products/reverse-vending-machines/DRS-11",
-        src:"/images/RVM_pro_729x344_1.jpg"
+        src:"/images/RVM_pro_765x430_6.jpg"
      },
      {
         title:"RVM DRS 22",
         subtitle:"The fiber-based solid-state laser, TruFiber, is the precision laser for delicate work. It stands out with its single-mode beam quality and the variety of power classes available. While the lower power classes of the fiber laser (up to 1 kW) are optimally suited for laser precision cutting and welding, the higher power classes (over 1 kW) shine when it comes to wobble welding.",
         link:"/products/reverse-vending-machines/DRS-22",
-        src:"/images/RVM_drs_22_1440x678.jpg"
+        src:"/images/RVM_drs_22_765x430.jpg"
      },
      {
         title:"RVM DRS 35",
         subtitle:"The fiber-based solid-state laser, TruFiber, is the precision laser for delicate work. It stands out with its single-mode beam quality and the variety of power classes available. While the lower power classes of the fiber laser (up to 1 kW) are optimally suited for laser precision cutting and welding, the higher power classes (over 1 kW) shine when it comes to wobble welding.",
         link:"/products/reverse-vending-machines/DRS-35",
-        src:"/images/RVM_DRS_35_500x220.jpg"
+        src:"/images/RVM_DRS_35_765x430.jpg"
      }
 ]
