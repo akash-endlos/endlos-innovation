4b1047438a1677d092bb48881a000aac6bb8f463
akash-endlos <akash.endlos@gmail.com>
2023-06-01 10:55:05 +0530

    updated

--- src/app/components/page-components/image-card-container/ImageCardContainer.js
+++ src/app/components/page-components/image-card-container/ImageCardContainer.js
@@ -38,35 +38,3 @@ const ImageCardContainer = ({ data }) => {
 };
 
 export default ImageCardContainer;
-
-
-{/* <div className="w-100 py-5">
-  <div className="container px-5">
-    <div className="d-flex justify-content-center">
-      <div className="row">
-        {data?.length > 0 &&
-          data?.map((item) => (
-            <div className="col-xl-6">
-              <h4>{item.title}</h4>
-              <p className="w-full text-height">{item.subtitle}</p>
-              <div className="position-relative pb-5 mb-5">
-                <div className="inner">
-                  <img
-                    src={item.img_src}
-                    alt="img1"
-                    className="image-container"
-                  />
-                </div>
-                <div className="arrow-on-image pointer">
-                  <Link href={item.link}>
-                    {" "}
-                    <FaArrowCircleRight size={60} />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-    </div>
-  </div>
-</div> */}
