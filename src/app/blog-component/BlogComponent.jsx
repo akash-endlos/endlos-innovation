718be77dca0037977b1bcc36b42f19b789e52c0e
akash-endlos <akash.endlos@gmail.com>
2023-06-01 11:14:58 +0530

    updated

--- src/app/blog-component/BlogComponent.jsx
+++ src/app/blog-component/BlogComponent.jsx
@@ -12,28 +12,32 @@ const BlogComponent = () => {
                     <h2 className='text-muted text-center'>Blog</h2>
                     <div className="d-flex justify-content-center">
                         <div className="row">
-                            {blogContent.map((item, index) => (
-                                <div key={index} className='col-xl-4 py-5'>
-                                    <div className="blog-item">
-                                        <div style={{ height: "350px" }}>
-                                            <h4>{item.title}</h4>
-                                            <p className='justify'>{item.content}</p>
-                                        </div>
-                                        <div className='position-relative'>
-                                            <div className="inner">
-                                                <img
-                                                    src="/assets/img/blog/3.jpg"
-                                                    alt="img1"
-                                                    className="image-container"
-                                                />
-                                            </div>
-                                            <div className="blog-arrow-on-image ">
-                                                <FaArrowCircleRight size={60} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
+                        {blogContent.map((item, index) => (
+  <div key={index} className='col-xl-4 py-5'>
+    <div className="blog-item">
+      <div className="content-container">
+        <h4>{item.title}</h4>
+        <p className='justify'>{item.content}</p>
+      </div>
+      <div className='position-relative'>
+        <div className="inner">
+          <img
+            src="/assets/img/blog/3.jpg"
+            alt="img1"
+            className="image-container img-fluid"
+          />
+        </div>
+        <div className="blog-arrow-on-image">
+          <FaArrowCircleRight size={60} />
+        </div>
+      </div>
+    </div>
+  </div>
+))}
+
+
+
+
 
                         </div>
                     </div>
