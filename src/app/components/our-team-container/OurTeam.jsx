e4c479f0848f659125f5b3711d5a0cf1bd7c0c6f
akash-endlos <akash.endlos@gmail.com>
2023-06-12 17:17:03 +0530

    updated

--- src/app/components/our-team-container/OurTeam.jsx
+++ src/app/components/our-team-container/OurTeam.jsx
@@ -9,20 +9,20 @@ const OurTeam = ({ team }) => {
         <div className="col-xl-6">
           <div className="card-deck">
             <div class="card text-center">
-              <div className="our-team-image">
+              <div className="our-team-image pt-5">
                 <img
-                  className="img-responsive w-100"
+                  className="img-responsive rounded-circle w-50 h-75"
                   src={member.src}
-                  alt="Card image cap"
+                  alt="Avatar"
                 />
               </div>
               <div className="card-body">
-              <div className="d-flex gap-2 align-items-center">
-              {member.linkedin ? <Link  target="_blank" href={member.linkedin} className="pointer"><img src="/images/linkedin.svg" height={25} width={25} /></Link> :""}
-              {member.twitter ? <Link  target="_blank"  href={member.twitter} className="pointer"><img src="/images/twitter.svg" height={25} width={25} /></Link> :""}
-              </div>
-              <h5 className={`${member.linkedin ? "":"pt-4"}`}>{member.title}</h5>
-              <p>{member.subtitle}</p> 
+                <div className="d-flex gap-2 align-items-center">
+                  {member.linkedin ? <Link target="_blank" href={member.linkedin} className="pointer"><img src="/images/linkedin.svg" height={25} width={25} /></Link> : ""}
+                  {member.twitter ? <Link target="_blank" href={member.twitter} className="pointer"><img src="/images/twitter.svg" height={25} width={25} /></Link> : ""}
+                </div>
+                <h5 className={`${member.linkedin ? "" : "pt-4"}`}>{member.title}</h5>
+                <p>{member.subtitle}</p>
                 {/* <p className={`${member.find_out_more ? "":"pt-4"}`} >{member.subtitle}</p> */}
                 {/* {member.find_out_more ? <span class="our-team-member-btn"><Link href={member.link}>Find Out More</Link></span> :""} */}
               </div>
