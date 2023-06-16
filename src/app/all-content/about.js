3c779307de9095d7dbcaf214bece511578e1bcb4
akash-endlos <akash.endlos@gmail.com>
2023-06-16 18:11:41 +0530

    updated

--- src/app/all-content/about.js
+++ src/app/all-content/about.js
@@ -3,21 +3,21 @@ export const AboutImageCardContainer = [
     title: "Our Mission",
     subtitle:
       "Our mission is to offer eco-friendly products and services that are cutting-edge and long-lasting and help make the world a better place. To solve the escalating   environmental   issues   our   society   faces,   we   work   to   be   theindustry leader in eco-friendly goods and services",
-    img_src: "/assets/img/blog/3.jpg",
+    img_src: "/assets/img/our-mission.jpg",
     link: "",
   },
   {
     title: "Our Vision",
     subtitle:
       "Our   vision   is   to   establish   a   society   that   conserves   natural   resources,reduces waste, and protects the environment for coming generations. In the future, we want to see our goods and services consumed broadly and help build a more circular and sustainable economy.",
-    img_src: "/assets/img/blog/3.jpg",
+    img_src: "/assets/img/our-vision.jpg",
     link: "",
   },
   {
     title: "Our Core Values",
     subtitle:
       "We prioritize sustainability and innovation to reduce environmental impact and   promote   a   circular   economy.   We   deliver   high-quality   products   and services to meet customer needs and environmental demands, treating all with respect, fairness, and integrity.",
-    img_src: "/assets/img/blog/3.jpg",
+    img_src: "/assets/img/our-core-values.jpg",
     link: "",
   },
 ];
