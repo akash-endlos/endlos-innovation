cabbb0714fd2768bf2026cd1a2a3f0635031c663
akash-endlos <akash.endlos@gmail.com>
2023-06-16 11:58:00 +0530

    updated

--- src/app/blog-component/BlogComponent.jsx
+++ src/app/blog-component/BlogComponent.jsx
@@ -6,67 +6,66 @@ import { blogContent } from '../all-content/blogContent';
 
 const BlogComponent = () => {
   return (
-    <>
-
-<div class="container pt-5">
-        <div class="row">
-        {blogContent.map((item, index) => (
-                <div key={index} className='col-xl-4 py-5'>
-                  <div className="blog-item">
-                    <h4 className='justify pb-4 h-50'>{item.title}</h4>
-                    <div className='pb-5'>
-                      <div className='position-relative'>
-                        <div className='inner'>
-                          <img
-                            src={item.img_src}
-                            alt="img1"
-                           className='blog-image-container '
-                          />
-                        </div>
-                        {/* <div className="blog-arrow-on-image">
-                          <FaArrowCircleRight size={60} />
-                        </div> */}
-                      </div>
-                    </div>
-                    <p className='justify'>{item.content}</p>
-                  </div>
-                </div>
-              ))}
-        </div>
-      </div>
-      {/* <div className="w-100 py-5">
-        <div className="container px-5">
-          <h2 className='text-muted text-center'>Blog</h2>
-          <div className="d-flex justify-content-center">
-            <div className="row">
-              {blogContent.map((item, index) => (
-                <div key={index} className='col-xl-4 py-5'>
-                  <div className="blog-item">
-                    <h4 className='justify pb-4'>{item.title}</h4>
-                    <div className='pb-5'>
-                      <div className='position-relative'>
-                        <div className="inner">
-                          <img
-                            src="/assets/img/blog/3.jpg"
-                            alt="img1"
-                            className="image-container img-fluid"
-                          />
-                        </div>
-                        <div className="blog-arrow-on-image">
-                          <FaArrowCircleRight size={60} />
-                        </div>
-                      </div>
-                    </div>
-                    <p className='justify'>{item.content}</p>
-                  </div>
-                </div>
-              ))}
+    <div >
+      {blogContent.map((item, index) => (
+        <div class="container py-5">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <div class="d-flex align-items-center inner">
+                <img src={item.img_src} alt="img1" class="blog-image-container img-fluid" />
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-6">
+              <h4 class="justify h-auto">{item.title}</h4>
+              <p class="justify">{item.content}</p>
             </div>
           </div>
         </div>
-      </div> */}
-    </>
+
+      ))}
+    </div>
   )
 }
 
 export default BlogComponent
+
+
+
+// import Image from 'next/image';
+// import React from 'react'
+// import { FaArrowCircleRight } from "react-icons/fa";
+// import './blog.css'
+// import { blogContent } from '../all-content/blogContent';
+
+// const BlogComponent = () => {
+//   return (
+//     <>
+
+// <div class="container pt-5">
+//         <div class="row">
+//         {blogContent.map((item, index) => (
+//                 <div key={index} className='col-xl-4 py-5'>
+//                   <div className="blog-item">
+//                     <h4 className='justify pb-4 h-50'>{item.title}</h4>
+//                     <div className='pb-5'>
+//                       <div className='position-relative'>
+//                         <div className='inner'>
+//                           <img
+//                             src={item.img_src}
+//                             alt="img1"
+//                            className='blog-image-container '
+//                           />
+//                         </div>
+//                       </div>
+//                     </div>
+//                     <p className='justify'>{item.content}</p>
+//                   </div>
+//                 </div>
+//               ))}
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default BlogComponent
