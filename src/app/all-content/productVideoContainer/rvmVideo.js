ffcc34e4128f8fe6d9cbc8c2a242a5a67715c564
akash-endlos <akash.endlos@gmail.com>
2023-06-20 10:08:41 +0530

    updated

--- src/app/all-content/productVideoContainer/rvmVideo.js
+++ src/app/all-content/productVideoContainer/rvmVideo.js
@@ -1,41 +1,61 @@
 export const rvmEcoVideo = [
     {
-        video_src: "/assets/1.mp4",
+        video_src: "/assets/products/eco/RVM_eco.mp4",
+        video_desc: "Energy-efficient process cooler"
+    },
+    {
+        video_src: "/assets/products/eco/RVM_eco_bin_video.mp4",
+        video_desc: "Energy-efficient process cooler"
+    },
+    {
+        video_src: "/assets/products/eco/RVM_eco_full_video.mp4",
+        video_desc: "Energy-efficient process cooler"
+    },
+    {
+        video_src: "/assets/products/eco/Plastic_bottle_passsing.mp4",
+        video_desc: "Energy-efficient process cooler"
+    },
+    {
+        video_src: "/assets/products/eco/plastic_bottle_falling.mp4",
         video_desc: "Energy-efficient process cooler"
     },
 ]
 
 export const rvmProVideo = [
     {
-        video_src: "/assets/1.mp4",
+        video_src: "/assets/products/eco/Plastic_bottle_passsing.mp4",
         video_desc: "Energy-efficient process cooler"
     },
 ]
 
 export const rvmPremiumVideo = [
     {
-        video_src: "/assets/1.mp4",
+        video_src: "/assets/products/premium/RVM_premium_bin_video.mp4",
+        video_desc: "Energy-efficient process cooler"
+    },
+    {
+        video_src: "/assets/products/premium/RVM_premium_full_video.mp4",
         video_desc: "Energy-efficient process cooler"
     },
 ]
 
 export const rvmDRS11Video = [
     {
-        video_src: "/assets/1.mp4",
+        video_src: "/assets/products/eco/Plastic_bottle_passsing.mp4",
         video_desc: "Energy-efficient process cooler"
     },
 ]
 
 export const rvmDRS22Video = [
     {
-        video_src: "/assets/1.mp4",
+        video_src: "/assets/products/eco/Plastic_bottle_passsing.mp4",
         video_desc: "Energy-efficient process cooler"
     },
 ]
 
 export const rvmDRS35Video = [
     {
-        video_src: "/assets/1.mp4",
+        video_src: "/assets/products/eco/Plastic_bottle_passsing.mp4",
         video_desc: "Energy-efficient process cooler"
     },
 ]
